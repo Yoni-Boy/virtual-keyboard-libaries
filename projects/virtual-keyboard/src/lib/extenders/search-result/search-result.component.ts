5db910b854c6e8fee3c29cf6f60f54eef71dbe3f
Jonathan Nechemia <yoniavaryoni@gmail.com>
2024-01-11 16:06:20 +0200

    update

--- projects/virtual-keyboard/src/lib/extenders/search-result/search-result.component.ts
+++ projects/virtual-keyboard/src/lib/extenders/search-result/search-result.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { VirtualKeyboardModule } from "../../virtual-keyboard.module";
+import { KeyboardLayout } from 'virtual-keyboard';
 
 @Component({
-  selector: 'vk-search-result',
-  standalone: true,
-  imports: [],
-  templateUrl: './search-result.component.html',
-  styleUrl: './search-result.component.css'
+    selector: 'vk-search-result',
+    standalone: true,
+    templateUrl: './search-result.component.html',
+    styleUrl: './search-result.component.css',
+    imports: [VirtualKeyboardModule]
 })
 export class SearchResultComponent {
+
+  @Input() keyboardLayout: KeyboardLayout | undefined;
+  @Input() language: string | undefined;
+  //This variable declare the virtual keyboard id. 
+  //With this parameter we notify the 'acceptWithIDCallBack' with this id param.
+  //For example when we declare the call back method at the father, and we click on 'accept' button then we will 
+  //Execute this method with this parameter. With this param We can know who is element    
+  @Input() vk_id: string | undefined;
+  @Input() validateCallBack!: (args: string) => boolean;
+  @Input() acceptCallBack!: (args: string) => boolean | void;
+  @Input() acceptWithIDCallBack!: (vk_id: string,text: string) => any | void;
+
 
 }
--- projects/virtual-keyboard/src/public-api.ts
+++ projects/virtual-keyboard/src/public-api.ts
@@ -10,3 +10,4 @@ export * from './lib/interfaces/interfaces';
 export * from './lib/interfaces/keyboards-layout/keyboard-layout';
 export * from './lib/services/virtual-keyboard-events.service';
 export * from './lib/virtual-keyboard.module';
+export * from './lib/extenders/search-result/search-result.component';
