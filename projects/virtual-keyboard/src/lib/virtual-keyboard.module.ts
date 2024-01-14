f31ae7ab327ab2e032631f441f687473f22bd78b
Jonathan Nechemia <yoniavaryoni@gmail.com>
2024-01-14 09:37:27 +0200

    update

--- projects/virtual-keyboard/src/lib/virtual-keyboard.module.ts
+++ projects/virtual-keyboard/src/lib/virtual-keyboard.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { VirtualKeyboardComponent } from './virtual-keyboard.component';
 import {VirtualKeyboardEventsService} from './services/virtual-keyboard-events.service';
 import { CommonModule } from '@angular/common';
+import { SearchResultComponent } from '../public-api';
 //https://stackoverflow.com/questions/60102146/custom-library-component-is-not-a-known-element-but-app-compiles-and-runs
 
 
 @NgModule({
   declarations: [
-    VirtualKeyboardComponent
+    VirtualKeyboardComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SearchResultComponent
   ],
   exports: [
-    VirtualKeyboardComponent
+    VirtualKeyboardComponent,
+    SearchResultComponent
   ],
   providers: [VirtualKeyboardEventsService]
 })
